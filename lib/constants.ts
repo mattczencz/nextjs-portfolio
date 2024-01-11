3ce3d868b1058ad7701ae9418ad4517a991e008f
Matt Czencz <mattczenczdev@gmail.com>
2024-01-11 13:26:55 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -42,7 +42,7 @@ export const workProjects: Project[] = [
     title: 'TaskFlow',
     description: "Leveraging the power of Next.js 14, Prisma, and MySQL, I built a robust and scalable platform that seamlessly manages tasks. The app's sleek and intuitive user interface is powered by Radix UI, providing a seamless user experience. This project showcases my proficiency in full-stack development, from designing interactive interfaces to implementing efficient backend solutions. Check out this project in my portfolio to witness firsthand how I bring innovation and functionality together in the digital realm.",
     image: TaskflowPhoto,
-    codeLink: 'https://github.com/mattczencz/taskflow',
+    codeLink: 'https://github.com/mattczencz/trackflow',
     liveLink: 'https://trackflow-mattczencz.vercel.app/',
     languages: [
       'Next.js 14',
